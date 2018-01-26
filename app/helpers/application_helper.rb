5d410b18ad3ad779b840210d407cc17d19e4887e
Daichi Tanaka <tanakadaichi_1989@MacBook-Air.local>
2018-01-26 20:10:03 +0900

    Edit home/top.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def full_page(page_title="")
+        base_title = "TokyoMetroAPI & Ruby on Rails"
+        
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
