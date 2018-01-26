2eaf9c3f9f621f25e53e09a0a0871cbe25a04489
Daichi Tanaka <tanakadaichi_1989@MacBook-Air.local>
2018-01-26 18:53:09 +0900

    APIからの情報取得方法を@hashに変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,21 +5,56 @@ class HomeController < ApplicationController
       require 'net/http'
       require 'date'
       
-      
-      consumerkey = Rails.application.secrets.tokyometro_key
-      
       uri = URI.parse("https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:TrainInformation&acl:consumerKey=#{ENV["TOKYOMETRO_APIKEY"]}")
-      
       json = Net::HTTP.get(uri)
-      @result = JSON.parse(json)
+      result = JSON.parse(json)
       
-      puts @result
+      @hash = Hash.new()
       
       (0..8).each do |i|
-          puts @result[i]["odpt:railway"] + "  " + @result[i]["odpt:trainInformationText"]
+          @hash[result[i]["odpt:railway"]] = result[i]["odpt:trainInformationText"]
       end
+      
+      puts "-----------------------------------"
+      
+      puts "《銀座線》"
+      
+      puts @hash["odpt.Railway:TokyoMetro.Ginza"]
+      
+      puts "《丸の内線》"
+      
+      puts @hash["odpt.Railway:TokyoMetro.Marunouchi"]
+      
+      puts "《日比谷線》"
+      
+      puts @hash["odpt.Railway:TokyoMetro.Hibiya"]
+      
+      puts "《東西線》"
+      
+      puts @hash["odpt.Railway:TokyoMetro.Tozai"]
+      
+      puts "《千代田線》"
+      
+      puts @hash["odpt.Railway:TokyoMetro.Chiyoda"]
+      
+      puts "《有楽町線》"
+      
+      puts @hash["odpt.Railway:TokyoMetro.Yurakucho"]
+      
+      puts "《半蔵門線》"
+      
+      puts @hash["odpt.Railway:TokyoMetro.Hanzomon"]
+      
+      puts "《南北線》"
+      
+      puts @hash["odpt.Railway:TokyoMetro.Namboku"]
+      
+      puts "《副都心線》"
+      
+      puts @hash["odpt.Railway:TokyoMetro.Fukutoshin"]
+      
+      puts "-----------------------------------"
       
       
   end
-
   end
