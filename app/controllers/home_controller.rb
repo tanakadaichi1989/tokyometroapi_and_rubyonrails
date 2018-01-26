539bf830ac5664d4a53f53365ff0fdaaf9b4266e
Daichi Tanaka <tanakadaichi_1989@MacBook-Air.local>
2018-01-26 16:16:21 +0900

    環境変数の導入

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,12 +8,12 @@ class HomeController < ApplicationController
       
       consumerkey = Rails.application.secrets.tokyometro_key
       
-      uri = URI.parse("https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:TrainInformation&acl:consumerKey=#{API KEY}")
+      uri = URI.parse("https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:TrainInformation&acl:consumerKey=#{ENV["TOKYOMETRO_APIKEY"]}")
       
       json = Net::HTTP.get(uri)
       @result = JSON.parse(json)
       
-      puts @result[0]
+      puts @result
       
       (0..8).each do |i|
           puts @result[i]["odpt:railway"] + "  " + @result[i]["odpt:trainInformationText"]
