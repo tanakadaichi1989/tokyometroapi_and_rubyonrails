83ed2637434b7cc03747afae14f282f59dbeea59
Daichi Tanaka <tanakadaichi_1989@MacBook-Air.local>
2018-01-28 06:00:27 +0900

    refactoring controllers

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -8,9 +8,7 @@ class TimetablesController < ApplicationController
   end
   
   def search
-      uri = URI.parse("https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:StationTimetable&acl:consumerKey=#{ENV["TOKYOMETRO_APIKEY"]}")
-      json = Net::HTTP.get(uri)
-      results = JSON.parse(json)
+      results = set_request_timetable
       
       a = 302
       
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,18 @@ module ApplicationHelper
     def now_time
         DateTime.now.strftime("%Y/%m/%d %H:%M")
     end
+    
+    def set_request_train_information
+        uri = URI.parse("https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:TrainInformation&acl:consumerKey=#{ENV["TOKYOMETRO_APIKEY"]}")
+        json = Net::HTTP.get(uri)
+        results = JSON.parse(json)
+        
+        return results
+    end
+    
+    def set_request_timetable
+        uri = URI.parse("https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:StationTimetable&acl:consumerKey=#{ENV["TOKYOMETRO_APIKEY"]}")
+        json = Net::HTTP.get(uri)
+        results = JSON.parse(json)
+    end
 end
