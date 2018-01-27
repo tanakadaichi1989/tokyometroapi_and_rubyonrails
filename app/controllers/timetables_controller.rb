9a1260c3d8fc00f5e69e3d362233fdb8ff13850c
Daichi Tanaka <tanakadaichi_1989@MacBook-Air.local>
2018-01-28 06:23:44 +0900

    add search function by params[:number]

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -8,7 +8,7 @@ class TimetablesController < ApplicationController
   def search
       results = set_request_timetable
       
-      a = params[:number]
+      a = params[:number].to_i
       
       @timetables = results[a]["odpt:weekdays"]
       puts results[a]
