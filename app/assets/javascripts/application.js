99c2044652f4d0ef8344d66dbb1f7e7402393ed7
Daichi Tanaka <tanakadaichi_1989@MacBook-Air.local>
2018-01-28 20:22:31 +0900

    finish linked pulldown edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,35 @@
 //= require turbolinks
 //= require_tree .
 
+$(function(){
+  //路線名選択
+  $('select[name="line_list"]').change(function(){
+  //選択した路線のクラス名を取得
+  var line_class = $('select[name="line_list"] option:selected').attr("class");
+  console.log(line_class);
+
+  var count = $('select[name="station_list"]').children().length;
+  console.log(count);
+
+  for(a=0; a<count; a++){
+        var station_list = $('select[name="station_list"] option:eq('+a+')');
+        if(station_list.attr("class") === line_class){
+          station_list.show();
+        } else {
+          station_list.hide();
+      }
+      }
+
+      for(b=0; b<count; b++){
+        var direction_list = $('select[name="direction_list"] option:eq('+b+')');
+        if(direction_list.attr("class") === line_class){
+          direction_list.show();
+        } else {
+          direction_list.hide();
+      }
+      }
+
+  });
+
+
+});
